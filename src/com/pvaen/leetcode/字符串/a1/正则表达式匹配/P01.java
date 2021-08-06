4dbfd03063ebf63ed8359a6c49ab693ad384122d
pvaenzzh <179714206@qq.com>
2021-08-06 23:21:52 +0800

    多种可能性问题, 需要使用动态规划

---
+++ src/com/pvaen/leetcode/字符串/a1/正则表达式匹配/P01.java
@@ -0,0 +1,23 @@
+package com.pvaen.leetcode.字符串.a1.正则表达式匹配;
+
+
+
+public class P01 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public boolean isMach(String s, String p) {
+//        char point = '.', any = '*';
+//        ArrayList<String> objects = new ArrayList<>();
+//        for (int i = 0; i < p.length(); i++) {
+//
+//        }
+//
+//
+//
+//    }
+
+
+}
